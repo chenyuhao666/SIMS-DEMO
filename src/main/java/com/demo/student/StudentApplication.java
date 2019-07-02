360c60c9001a2653275f74339aad50619ea51a77
chenyuhao <1305816737@qq.com>
2019-07-02 10:26:59 +0800

    test

---
+++ src/main/java/com/demo/student/StudentApplication.java
@@ -0,0 +1,17 @@
+package com.demo.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenyuhao
+ * @date 2019-07-02
+ */
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
